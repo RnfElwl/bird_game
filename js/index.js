19d0215a6e3df8c8d31ef7fb2ad4a0c08d6be4f7
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2023-05-12 23:55:36 +0900

    bird jump

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 class Bird {
   constructor() {
     this.bird = document.querySelector(".bird");
+    this.map = document.querySelector(".map");
     this.birdTop = this.bird.getBoundingClientRect().y;
+    this.isJump = false;
+    this.speed = 1.5;
+
     this.event();
   }
   event() {
     setInterval(this.setBirdTop.bind(this), 10);
+    this.map.addEventListener("click", this.handleFlyBird.bind(this));
   }
   setBirdTop() {
-    console.log(this.birdTop);
-    this.birdTop += 1;
+    this.speed += 0.03;
+    this.birdTop += 1 + this.speed;
+    this.bird.style.top = this.birdTop + "px";
+  }
+  handleFlyBird() {
+    this.speed = 1;
+    this.birdTop -= 500 - this.speed;
     this.bird.style.top = this.birdTop + "px";
   }
 }

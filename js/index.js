00cd766064bc4040d8513fd7c88ccb9b4db715df
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2023-06-06 20:29:02 +0900

    파이프 생성 및 삭제

--- js/index.js
+++ js/index.js
@@ -9,36 +9,60 @@ class Bird {
     this.speed = 5.5;
     this.gameover = this.map.getBoundingClientRect().height;
     this.time = null;
+    //새관련 변수들
+
+    this.pipe = null;
+    this.pipeSpeed = 1100;
+    this.pipeTime = Math.round(Math.random() * (5 - 3) + 3);
     this.event();
   }
 
   event() {
     this.bird.style.transition = "top 0.5s";
     this.time = setInterval(this.setBirdTop.bind(this), 50);
+    this.pipe = setInterval(this.setPipe.bind(this), this.pipeTime * 1000);
     this.map.addEventListener("click", this.handleFlyBird.bind(this));
   }
   setBirdTop() {
     this.speed += 0.7;
     this.birdTop += 2 + this.speed;
-    const a = this.bird.getBoundingClientRect().y - this.mapTop;
-    if (a >= this.gameover - 50) {
+    const die = this.bird.getBoundingClientRect().y - this.mapTop;
+    if (die >= this.gameover - 50) {
       console.log("game over");
-      this.birdTop = this.gameover - 50;
-      this.startBtn.style.display = "block";
-      this.bird.style.transition = "none";
-      clearInterval(this.time);
+      this.setGameOver();
     }
     this.bird.style.top = this.birdTop + "px";
+  }
+  setGameOver() {
+    this.birdTop = this.gameover - 50;
+    this.startBtn.style.display = "block";
+    this.bird.style.transition = "none";
+    clearInterval(this.time);
+    clearInterval(this.pipe);
   }
   handleFlyBird() {
     this.speed = 5;
     this.birdTop -= 400 - this.speed;
   }
-}
-
-class Pipe {
-  constructor() {
-    this.copyPipeTop;
-    this.copyPipebottom;
+  setPipe() {
+    console.log("hi");
+    this.pipeTime = Math.round(Math.random() * (5 - 3) + 3);
+    this.pipeEl = document.querySelector(".clone_pipe").cloneNode();
+    this.copyPipeTop = document.querySelector(".clone_pipe-top").cloneNode();
+    this.copyPipeBottom = document
+      .querySelector(".clone_pipe-bottom")
+      .cloneNode();
+    const pipeList = document.querySelectorAll(".map .clone_pipe");
+    console.log(pipeList);
+    if (pipeList.length >= 3) {
+      const firstNode = pipeList[0];
+      firstNode.parentNode.removeChild(firstNode);
+    }
+    this.copyPipeTop.style.height = 70 + "px";
+    this.copyPipeBottom.style.height = 70 + "px";
+    this.pipeEl.appendChild(this.copyPipeTop);
+    this.pipeEl.appendChild(this.copyPipeBottom);
+    this.map.appendChild(this.pipeEl);
+    this.pipeEl.style.right = this.pipeSpeed + "px";
   }
 }
